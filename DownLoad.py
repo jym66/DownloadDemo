d967b90c8bea4b8f84a533f0a3aeecaac11c6785
13901 <1390109557@qq.com>
2020-05-25 12:13:40 +0800

    fix

--- DownLoad.py
+++ DownLoad.py
@@ -2,19 +2,19 @@ import requests
 import threading
 import time
 import hashlib
-import os
 
 
-class Down:
+class Downs:
     """简易下载器 Demo """
 
     def __init__(self):
+        self.session = requests.Session()  # Tcp连接复用
         self.__url = None
         self.__verify = True
         self.__size = None
         self.__name = None
         self.__back_name = None
-        self.__thread = 1
+        self.__thread = 128
         self.__data_count = 0
         self.__flag = False
         self.__extend_name = None
@@ -29,8 +29,8 @@ class Down:
         self.__type = ['html', 'png', 'jpeg']
         self.plugin = []
         self.__headers = {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) '
-                          'Chrome/63.0.3239.84 Safari/537.36',
+            'User-Agent': 'netdisk;P2SP;2.2.60.26',
+            "Cookie": ""
         }
 
     def set_cookie(self, cookie: str):
@@ -41,9 +41,19 @@ class Down:
 
     def set_filename(self, name: str):
         self.__name = name
+        self._chuck_name()
 
     def set_isMd5(self, md5: bool):
         self.__isMd5 = md5
+
+    def set_file_size(self, size):
+        self.__size = size
+
+    def set_headers(self, headers: dict):
+        self.__headers = headers
+
+    def set_thread(self, thread):
+        self.__thread = thread
 
     def get_flag(self):
         return self.__flag
@@ -51,17 +61,20 @@ class Down:
     def _get_response_headers(self):
         """获取服务器响应信息"""
         print("玩命获取数据中....\n")
-        response = requests.head(self.__url, headers=self.__headers, verify=self.__verify)
+        response = self.session.head(self.__url, headers=self.__headers, verify=self.__verify)
         if str(response.status_code)[0] == '4' or str(response.status_code)[0] == '5':
-            raise Exception("下载失败 错误码 {},请检查是否有访问权限,或者尝试带上cookie".format(response.status_code))
+            raise Exception("响应头获取失败 错误码 {},请检查是否有访问权限,或者尝试带上cookie".format(response.status_code))
         """请求为302是 则迭代找寻源文件"""
         while response.status_code == 302:
             if 'Location' in response.headers:
                 self.__url = response.headers['Location']
             else:
                 raise Exception("获取原始地址失败！")
-            response = requests.head(self.__url, headers=self.__headers, verify=self.__verify)
+            response = self.session.head(self.__url, headers=self.__headers, verify=self.__verify)
+            if response.status_code == 302:
+                continue
             if 'Content-Length' in response.headers:
+
                 self.__response_headers = response
                 return
             else:
@@ -75,7 +88,8 @@ class Down:
 
     def _get_file_size(self):
         """获取文件大小"""
-        self.__size = self.__response_headers.headers['Content-Length']
+        if self.__size is None:
+            self.__size = self.__response_headers.headers['Content-Length']
 
     def _chuck_thread(self):
         """验证是否支持多线程"""
@@ -95,27 +109,31 @@ class Down:
                         if '.' not in self.__name:
                             self.__extend_name = i
 
-    def _get_file_name(self):
+    def _chuck_name(self):
+        """防止windows命名错误"""
         name_list = ['<', '>', '/', '\\', '|', ':', '"', '*', '?']
-        """获取文件名"""
-        if '/' in self.__url:
-            self.__name = self.__url.split('/')[-1][:255]
-            """防止windows命名错误"""
-            for i in name_list:
-                if i in self.__name:
-                    self.__name = self.__name.replace(i, '')
-            return
+        for i in name_list:
+            if i in self.__name:
+                self.__name = self.__name.replace(i, '')
+
+    def _get_file_name(self):
         """尝试在响应头获取文件名"""
         if 'Content-Disposition' in self.__response_headers.headers:
             self.__back_name = self.__response_headers.headers['Content-Disposition'].split("filename=")[-1]
+            print(self.__back_name)
             if '"' in self.__back_name:
                 self.__back_name = eval(self.__back_name)
             if self.__name is None:
                 self.__name = self.__back_name
+                return
+        """尝试在url获取文件名"""
+        if '/' in self.__url:
+            self.__name = self.__url.split('/')[-1][:255]
+        self._chuck_name()
 
     def _text_download(self):
         """文本文件下载函数,一般用不到"""
-        html = requests.get(self.__url, headers=self.__headers, stream=True, verify=self.__verify)
+        html = self.session.get(self.__url, headers=self.__headers, stream=True, verify=self.__verify)
         if html.status_code != '200' or html.status_code != '206':
             raise Exception("下载失败状态码 {}".format(html.status_code))
         with open(self.__name, "wb") as file:
@@ -127,10 +145,10 @@ class Down:
         # 支持多线程就更新请求头
         if self.__is_thread:
             headers.update({'Range': 'bytes={}-{}'.format(start, end)})
-        response = requests.get(self.__url, headers=headers, verify=self.__verify, stream=True)
+        response = self.session.get(self.__url, headers=headers, verify=self.__verify, stream=True)
         # 重试
         while str(response.status_code)[0] == '4' or str(response.status_code)[0] == '5':
-            response = requests.get(self.__url, headers=headers, verify=self.__verify, stream=True)
+            response = self.session.get(self.__url, headers=headers, verify=self.__verify, stream=True)
             time.sleep(2)
         with open(self.__name, "rb+") as file:
             # 再写入文件直接调整指针，多线程下载可避免文件错乱
@@ -147,6 +165,7 @@ class Down:
     def _create_file(self):
         """创建相同大小文件,获取文件大小之后调用"""
         with open(self.__name, "wb") as file:
+
             try:
                 file.truncate(int(self.__size))
             except OSError:
@@ -163,8 +182,6 @@ class Down:
             if i == self.__thread - 1:
                 end = int(self.__size)
             thread_list.append(threading.Thread(target=self._down_load, args=(start, end,)))
-            """进度条线程"""
-        threading.Thread(target=self._draw_able).start()
         for i in thread_list:
             i.start()
         for i in thread_list:
@@ -189,16 +206,15 @@ class Down:
     def _welcome(self):
         print("MD5校验:{} ".format(self.__isMd5))
         print("文件名:{} ".format(self.__name))
-        print("文件名大小:{} MB".format(int(int(self.__size) / 2 ** 20)))
+        print("文件大小:{} MB".format(int(int(self.__size) / 2 ** 20)))
         print("是否支持多线程: {}".format(self.__is_thread))
         print("线程数:{}".format(self.__thread))
         print("文件下载地址: {}".format(self.__url))
-        print("是否有cookie: {} ".format(bool(self.__cookie)))
-        print()
+        print("是否有cookie: {} \n".format(bool(self.__cookie)))
 
-    # 进度条
     def _draw_able(self):
-        """s = v/t 路程 = 速度 / 时间  下载速度计算应计算一个时间段内的速度"""
+
+        """进度条  s = v/t 路程 = 速度 / 时间  下载速度计算应计算一个时间段内的速度"""
         while not self.__flag:
             # 上一个时间段下载的数据
             start_time = int(time.time())
@@ -214,7 +230,7 @@ class Down:
             if speed < 1:
                 speed = speed * 2 ** 10
                 unit = 'kb/s'
-            print("\r文件下载进度 - > {}{:.2f}% ".format('>' * int(schedule), schedule) + " {:.2f}".format(speed) + unit,
+            print("\r文件下载进度 - > {:.2f}% ".format( schedule) + " {:.2f}".format(speed) + unit,
                   end=" ")
 
     def __chuck(self):
@@ -225,12 +241,6 @@ class Down:
         if self.__cookie is not None:
             self.__headers.update({"cookie": self.__cookie})
 
-    def registered_plugin(self):
-        """注册插件"""
-        for i in os.listdir('.\\plugin'):
-            if i.split('.')[-1] == 'py':
-                self.plugin.append(i.split('.')[-1])
-
     def go(self):
         """启动器"""
         self.__chuck()
@@ -238,17 +248,21 @@ class Down:
             self._get_response_headers()
             self._get_file_type()
             self._get_file_size()
-            self._chuck_thread()
+            # self._chuck_thread()
             if self.__name is None:
                 self._get_file_name()
         except Exception as e:
             print(e)
             return
+        if self.__name is None:
+            self._get_file_name()
         self._create_file()
-        self._welcome()
         if not self.__is_thread:
             self.__thread = 1
-            """判断文件类型启动相应的下载器"""
+        self._welcome()
+        """进度条线程"""
+        threading.Thread(target=self._draw_able).start()
+        """判断文件类型启动相应的下载器"""
         if self.__file_type in self.__type:
             self._text_download()
             self.__flag = True
@@ -260,9 +274,10 @@ class Down:
 
 
 if __name__ == '__main__':
-    url1 = "https://down.qq.com/qqweb/PCQQ/PCQQ_EXE/PCQQ2020.exe"
-    down = Down()
+    url1 = ""
+    down = Downs()
     down.set_url(url1)
-    # down.set_filename("1.m4s")
+    down.set_thread(4)
     down.set_isMd5(False)
+    # down.set_filename('123.zip')
     down.go()
